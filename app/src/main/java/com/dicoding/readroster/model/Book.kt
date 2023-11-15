46f201c171fe0ae85511708ba321c49d7bc6acf7
Arrizal28 <2010631170088@student.unsika.ac.id>
2023-11-15 12:05:13 +0700

    fix_add_resource

---
+++ app/src/main/java/com/dicoding/readroster/model/Book.kt
@@ -0,0 +1,9 @@
+package com.dicoding.readroster.model
+
+data class Book (
+    val id: Long,
+    val image: Int,
+    val author: String,
+    val title: String,
+    val description: String,
+)
---
+++ app/src/main/java/com/dicoding/readroster/model/FakeBookDataSource.kt
@@ -0,0 +1,78 @@
+package com.dicoding.readroster.model
+
+import com.dicoding.readroster.R
+
+object FakeBookDataSource {
+    val dummyBooks = listOf(
+        Book(
+            1,
+            R.drawable.book_1,
+            "Richar Wijaya",
+            "How to Get Rich with Online Business",
+            "A motivational book that provides tips and tricks on how to start, grow, and optimize an online business in the digital age."
+        ),
+        Book(
+            2,
+            R.drawable.book_2,
+            "Herlambang Wijaya",
+            "Inspirational Story: From Zero to Leader",
+            "A biography book that reveals the life and career of Indonesia's 7th President, from his childhood to becoming the nation's leader."
+        ),
+        Book(
+            3,
+            R.drawable.book_3,
+            "Andi Rahmat",
+            "Learn Python Easily",
+            "A guidebook that teaches the basics of Python programming in a practical, simple, and fun way."
+        ),
+        Book(
+            4,
+            R.drawable.book_4,
+            "Reza Nurfadillah",
+            "The Art of Drawing Manga",
+            "A tutorial book that teaches basic and advanced techniques in drawing manga, a popular Japanese comic style."
+        ),
+        Book(
+            5,
+            R.drawable.book_5,
+            "Dewi Lestari",
+            "Love is Never Wrong",
+            "A romantic novel that tells the love story between a doctor and a patient of different religions, cultures, and backgrounds."
+        ),
+        Book(
+            6,
+            R.drawable.book_6,
+            "Bambang Purwanto",
+            "Indonesian History: From Ancient Times to the Present",
+            "A history book that presents an overview of the development of Indonesia from pre-history to the present, with an emphasis on local and critical perspectives."
+        ),
+        Book(
+            7,
+            R.drawable.book_7,
+            "Eka Kurniawan",
+            "Horror in the Mental Hospital",
+            "A horror novel that tells the creepy experience of a psychologist working in a haunted and mysterious mental hospital."
+        ),
+        Book(
+            8,
+            R.drawable.book_8,
+            "Rizky Pratama",
+            "Mystery in Thousand Islands",
+            "Sebuah novel detektif yang mengisahkan petualangan seorang jurnalis muda yang terlibat dalam kasus pembunuhan di salah satu pulau wisata di Indonesia."
+        ),
+        Book(
+            9,
+            R.drawable.book_9,
+            "Rangga Aksara",
+            "Poetry Collection: Rain, Rainbows, and Love",
+            "A book of poetry that contains poetic works depicting various human emotions and feelings, especially about rain, rainbows, and love."
+        ),
+        Book(
+            10,
+            R.drawable.book_10,
+            "Achmad Zaini",
+            "Philosophy for Beginners",
+            "An introductory book that introduces important concepts and figures in the field of philosophy, with easy-to-understand language and relevant examples."
+        )
+    )
+}
